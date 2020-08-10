a62048a9d99f3377a96f625fb66d92a8008d4026
Albarra Erdanto <albarraerdanto@gmail.com>
2020-08-10 16:08:32 +0700

    final

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import "./components/app-bar.js";
+import "./styles/style.css";
 import main from "./views/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
--- src/components/film-item.js
+++ src/components/film-item.js
@@ -1,3 +1,4 @@
+import img from "../img/empty.png"
 class FilmItem extends HTMLElement {
     set film(film) {
         this._film = film;
@@ -7,7 +8,7 @@ class FilmItem extends HTMLElement {
     render() {
        this.innerHTML = `
        <div class="film-item">
-            <img src="${this._film.poster_path ? `http://image.tmdb.org/t/p/w500/${this._film.poster_path}` : "https://secureservercdn.net/50.62.198.70/157.58a.myftpupload.com/wp-content/uploads/2017/01/empty-featured2-1.png"}" alt="${this._film.title}">
+            <img src="${this._film.poster_path ? `http://image.tmdb.org/t/p/w500/${this._film.poster_path}` : img}" alt="${this._film.title}">
             <div class="film-info">
                 <h4>${this._film.title}</h4>
             </div>
